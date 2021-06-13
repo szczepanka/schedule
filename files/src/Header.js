c6dde027484ec2397444f79dbff1d0af82459222
Ania <58440008+szczepanka@users.noreply.github.com>
2021-06-13 12:38:49 +0200

    comeback

--- files/src/Header.js
+++ files/src/Header.js
@@ -4,12 +4,12 @@ import './header.css';
 function Header() {
   return (
     <div className="header">
-        <h1>Boards conference</h1>
-        <div className="info">
+      <h1>Boards conference</h1>
+      <div className="info">
        <button>Location</button> 
        <button>Contact</button> 
        <button>Manage</button> 
-        </div>
+      </div>
     </div>
   );
 }
